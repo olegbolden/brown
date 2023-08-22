37b9f1e4d12586731033725bb69d1f2fd6188c41
Oleg Boldenkov <oleg@bolden.ru>
2023-08-22 21:00:46 +0300

    Dashboard added

--- brown-app-ktor/src/jvmMain/kotlin/ru/otus/kotlin/brown/app/ktor/contollers/NotificationWsEventControllerV1.kt
+++ brown-app-ktor/src/jvmMain/kotlin/ru/otus/kotlin/brown/app/ktor/contollers/NotificationWsEventControllerV1.kt
@@ -1,5 +1,6 @@
 package ru.otus.kotlin.brown.app.ktor.contollers
 
+import io.ktor.utils.io.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.sync.Mutex
@@ -7,11 +8,21 @@ import kotlinx.coroutines.sync.withLock
 import ru.otus.kotlin.brown.api.v1.mappers.apiV1mapper
 import ru.otus.kotlin.brown.app.ktor.settings.AppSettings
 import ru.otus.kotlin.brown.biz.NotificationEventManager
+import ru.otus.kotlin.brown.common.models.NotificationEvent
 import ru.otus.kotlin.brown.common.models.NotificationEventState
 
 class NotificationWsEventControllerV1 {
     private val mutex = Mutex()
     private val sessions = mutableSetOf<WebSocketSession>()
+    private var error = false
+
+    private suspend fun sendMessage(eventList: Collection<NotificationEvent>) {
+        mutex.withLock {
+            sessions.forEach { session ->
+                session.send(Frame.Text(apiV1mapper.writeValueAsString(eventList)))
+            }
+        }
+    }
 
     suspend fun handle(session: WebSocketSession, appSettings: AppSettings) {
         mutex.withLock {
@@ -25,44 +36,17 @@ class NotificationWsEventControllerV1 {
         session.send(Frame.Text(apiV1mapper.writeValueAsString(currentEventList)))
 
         NotificationEventManager.getChannel().receiveAsFlow().collect { eventList ->
-            println("----------------------------------------")
-            println("Открытых сессий: ${sessions.size} ")
-            println("----------------------------------------")
             try {
-                mutex.withLock {
-                    sessions.forEach { session ->
-                        session.send(Frame.Text(apiV1mapper.writeValueAsString(eventList)))
+                sendMessage(eventList)
+            } catch (e: CancellationException) {
+                error = true
+            } finally {
+                if (error) {
+                    error = false
+                    mutex.withLock {
+                        sessions.removeIf { it == session }
                     }
-                }
-            } catch (error: Throwable) {
-                println("----------------------------------------")
-                println(error.message)
-                println("----------------------------------------")
-            } finally {
-                mutex.withLock {
-                    println("----------------------------------------")
-                    println("Client closed!")
-                    println("----------------------------------------")
-                    sessions.removeIf { it == session }
-                }
-            }
-        }
-        // Handle without flow destruction
-        session.incoming.receiveAsFlow().collect { it ->
-            val frame = it as? Frame.Text ?: return@collect
-            try {
-                val text = frame.readText()
-                println(text)
-            } catch (error: Throwable) {
-                println("----------------------------------------")
-                println(error.message)
-                println("----------------------------------------")
-            } finally {
-                mutex.withLock {
-                    println("----------------------------------------")
-                    println("Client closed!")
-                    println("----------------------------------------")
-                    sessions.removeIf { it == session }
+                    sendMessage(eventList)
                 }
             }
         }
